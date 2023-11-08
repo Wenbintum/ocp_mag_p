ac94eadde6595f65811ac7d98c562bc3c052120a
Wenbin Xu <wenxu@bc-d0-74-d-ac-bf.dhcp.lbnl.us>
2023-11-07 17:40:27 -0800

    ocp_mag_0

---
+++ sub_mp_4gpu_0E.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#SBATCH -C gpu 
+#SBATCH -A m4298_g
+#SBATCH --ntasks-per-node 4
+#SBATCH --cpus-per-task 32
+#SBATCH --gpus-per-node 4
+#SBATCH --time=10:00:00
+#SBATCH -N 1
+
+#export FI_MR_CACHE_MONITOR=userfaultfd
+IDENTIFIER=4_gpu_0.5_0E
+CONFIGPATH=/pscratch/sd/w/wenxu/jobs/OCP/ocp/configs/oc22/s2ef_mag/normal/dpp_normal_0E.yml
+LOGFILE=log_4gpu_0.5_0E
+NODES=4
+
+python -u -m torch.distributed.launch --nproc_per_node=$NODES main.py --distributed --num-gpus $NODES --mode train \
+       --config-yml $CONFIGPATH > $LOGFILE --identifier $IDENTIFIER
+
+#python -u -m torch.distributed.launch --master_addr="127.0.0.1" --master_port=29500 --nnodes=1 --node_rank=0 --nproc_per_node=4 \ 
+#  main.py --distributed --num-gpus 4 --mode train --config-yml configs/s2ef/200k/schnet/schnet.yml > log_multi 
+
+
